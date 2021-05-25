55ffcea5295301a47ae48ed50a5c2cd03deb9c83
Шалупин Данил <70798467+SD1999B52@users.noreply.github.com>
2021-05-25 19:39:19 +0500

    Add files via upload

--- Floyd_algorithm/Floyd_algorithm.java
+++ Floyd_algorithm/Floyd_algorithm.java
@@ -1,81 +1,64 @@
-/*
-Алгоритм Флойда
-*/
-
 class Floyd_algorithm {
-	static int inf = Integer.MAX_VALUE;//infinity
+	final static int inf = Integer.MAX_VALUE;
+	
+	static int[][] matrix = {
+		{ 0, 1, inf, inf, inf, inf, inf, inf, inf, inf, inf, inf },
+		{ inf, 0, 10, inf, inf, 5, inf, inf, inf, inf, 2, inf },
+		{ inf, inf, 0, inf, inf, inf, 3, inf, inf, inf, inf, inf },
+		{ inf, inf, 3, 0, inf, inf, 5, inf, inf, inf, inf, 5 },
+		{ inf, 5, inf, inf, 0, inf, inf, inf, inf, inf, inf, inf },
+		{ inf, inf, inf, inf, 3, 0, 7, inf, inf, inf, inf, inf },
+		{ inf, inf, inf, 7, inf, 8, 0, 4, inf, inf, 0, inf },
+		{ inf, inf, inf, inf, inf, inf, inf, 0, inf, inf, inf, 9 },
+		{ 11, inf, inf, inf, inf, 4, inf, inf, 0, inf, inf, 2 },
+		{ inf, inf, inf, inf, 1, inf, inf, inf, inf, 0, inf, inf },
+		{ inf, inf, inf, inf, inf, inf, inf, 2, inf, 1, 0, inf },
+		{ inf, inf, inf, inf, inf, inf, inf, inf, inf, inf, inf, 0 }
+	};
 	
 	public static void main( String[] args ) {
-		/*
-		int[][] array = {
-			{ 0, 8, 5 },
-			{ 3, 0, inf },
-			{ inf, 2, 0 }
-		};
-		*/
-		
-		int[][] array = {
-			{ 0, 1, inf, inf, inf, inf, inf, inf, inf, inf, inf, inf },
-			{ inf, 0, 10, inf, inf, 5, inf, inf, inf, inf, 2, inf },
-			{ inf, inf, 0, inf, inf, inf, 3, inf, inf, inf, inf, inf },
-			{ inf, inf, 3, 0, inf, inf, 5, inf, inf, inf, inf, inf },
-			{ inf, 5, inf, inf, 0, inf, inf, inf, inf, inf, inf, inf },
-			{ inf, inf, inf, inf, 3, 0, 7, inf, inf, inf, inf, inf },
-			{ inf, inf, inf, 7, inf, 8, 0, 4, inf, inf, 0, inf },
-			{ inf, inf, inf, inf, inf, inf, inf, 0, inf, inf, inf, 9 },
-			{ 11, inf, inf, inf, inf, 4, inf, inf, 0, inf, inf, 2 },
-			{ inf, inf, inf, inf, 1, inf, inf, inf, inf, 0, inf, inf },
-			{ inf, inf, inf, inf, inf, inf, inf, 2, inf, 1, 0, inf },
-			{ inf, inf, inf, inf, inf, inf, inf, inf, inf, inf, inf, 0 }
-		};
-		
-		int[][] previousPeaks = new int[array.length][array[0].length];
-		for ( int i = 0; i < previousPeaks.length; i++ ) {
-			for ( int i2 = 0; i2 < previousPeaks[0].length; i2++ ) {
-				previousPeaks[i][i2] = i2;
+		int[][] ancestorMatrix = new int[matrix.length][matrix[0].length];
+		for ( int i = 0; i < ancestorMatrix.length; i++ ) {
+			for ( int j = 0; j < ancestorMatrix[0].length; j++ ) {
+				ancestorMatrix[i][j] = j;
 			}
 		}
 		
-		outArray( array );
-		
-		for ( int k = 0; k < array.length; k++ ) {
-			for ( int i = 0; i < array.length; i++ ) {
-				for ( int j = 0; j < array[0].length; j++ ) {
-					if (( array[i][k] < inf ) & ( array[k][j] < inf )) {
-						int d = array[i][k] + array[k][j];
-						if ( array[i][j] > d ) {
-							array[i][j] = d;
-							previousPeaks[i][j] = previousPeaks[i][k];
+		for ( int k = 0; k < matrix.length; k++ ) {
+			for ( int i = 0; i < matrix.length; i++ ) {
+				for ( int j = 0; j < matrix[0].length; j++ ) {
+					if (( matrix[i][k] < inf ) & ( matrix[k][j] < inf )) {
+						if ( matrix[i][j] > matrix[i][k] + matrix[k][j] ) {
+							matrix[i][j] = matrix[i][k] + matrix[k][j];
+							ancestorMatrix[i][j] = ancestorMatrix[i][k];
 						}
 					}
 				}
 			}
-			outArray( array );
+			arrayOut( matrix );
+			System.out.println();
 		}
 		
-		getPath( previousPeaks, 0, 11 );
-	}
-	
-	//получить вершины через которые протекает путь
-	public static void getPath( int[][] previousPeaks, int start, int end ) {
-		System.out.print( start + " " );
+		//восстанавливаем маршрут
+		int start = 0, end = 11;
+		String way = "";
 		while ( start != end ) {
-			start = previousPeaks[start][end];
-			System.out.print( start + " " );
+			way += start + " -> ";
+			start = ancestorMatrix[start][end];
 		}
+		System.out.println( "Way: " + way + end );
 	}
 	
-	public static void outArray( int[][] array ) {
+	public static void arrayOut( int[][] array ) {
 		for ( int i = 0; i < array.length; i++ ) {
-			for ( int i2 = 0; i2 < array[0].length; i2++ ) {
-				if ( array[i][i2] == inf ) {
+			for ( int j = 0; j < array[0].length; j++ ) {
+				if ( array[i][j] == inf ) {
 					System.out.print( "inf " );
 				} else {
-					System.out.print( array[i][i2] + " " );
+					System.out.print( array[i][j] + " " );
 				}
 			}
 			System.out.println();
 		}
-		System.out.println();
 	}
 }
